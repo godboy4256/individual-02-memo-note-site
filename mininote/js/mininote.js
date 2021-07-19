8587d49ee149ea955309d45079e64bcff247c195
godboy4256 <82381946+godboy4256@users.noreply.github.com>
2021-07-19 19:42:21 +0900

    js update

--- mininote/js/mininote.js
+++ mininote/js/mininote.js
@@ -1,53 +1,96 @@
 const memoArea = document.querySelector('#memo-area')
-const memoTitleInput = document.querySelector('#memo-title-input')
-const memoMainInput = document.querySelector('#memo-main-input')
-const memoSave = document.querySelector('#memo-save')
-const memoList = document.querySelector('#memo-list')
-const memoPopUp = document.querySelector('#memo-popup')
-const memoTitle = document.querySelector('#memo-title')
-const memoMain = document.querySelector('#memo-main')
-const close = document.querySelector('#close')
-const memoLists = document.querySelectorAll('.memo-lists')
+     ,memoTitleInput = document.querySelector('#memo-title-input')
+     ,memoMainInput = document.querySelector('#memo-main-input')
+     ,memoSave = document.querySelector('#memo-save')
+     ,memoList = document.querySelector('#memo-list')
+     ,memoPopUp = document.querySelector('#memo-popup')
+     ,memoTitle = document.querySelector('#memo-title')
+     ,memoMain = document.querySelector('#memo-main')
+     ,memoDelete = document.querySelector('#delet-button')
+     ,close = document.querySelector('#close')
 
+     let n = 0
+     let memoSavingList = []
+     let checkTarget = [1,2]
 
 function noevent(event){
     event.preventDefault()
 }
 
+function deleteTarget(){
+   
+}
 
 function memoListsPopUp(){
    const innerMain = this.querySelector('.inner-main')
-   const innerTitle = this.querySelector('.inner-title')
-   const innerMainText = innerMain.innerText
-   const innerTitleText = innerTitle.innerText
+         ,innerTitle = this.querySelector('.inner-title')
+         ,innerMainText = innerMain.innerText
+         ,innerTitleText = innerTitle.innerText
    memoMain.innerText = innerMainText
    memoTitle.innerText = innerTitleText
 }
 
-function memoSaving(){
+function memoCreative(memoTitleInputValue,memoMainInputValue){
+
+    const ctMemoList = document.createElement('li')
+         ,ctdeleteinput = document.createElement('input')
+         ,ctTitleValue = document.createElement('span')
+         ,ctMainValue = document.createElement('span')
+         
+    ctTitleValue.innerText = memoTitleInputValue
+    ctMainValue.innerText = memoMainInputValue
+    ctdeleteinput.type = "checkbox"     
+    ctdeleteinput.addEventListener('change',function(){
+        this.parentElement.classList.toggle('del')
+        memoDelete.addEventListener('click',function(){
+         document.querySelectorAll('.del').forEach(item => {
+             item.remove()
+             memoSavingList = memoSavingList.filter((item2) => item2.id !== item.id)
+             localStorage.setItem('storagememosave',JSON.stringify(memoSavingList)) 
+         })
+        })
+    })
+    
+    ctTitleValue.classList.add('inner-title')
+    ctMainValue.classList.add('inner-main')
+    ctMemoList.append(ctdeleteinput,ctTitleValue,ctMainValue)
+    n++
+    ctMemoList.id = `ctmemo${n}`
+    const ctMemoObject  = {
+        id:ctMemoList.id,
+        ctt:memoTitleInputValue,
+        ctm:memoMainInputValue
+    }
+
+    const memoData = ctMemoObject
+    memoSavingList.push(memoData)
+    const memusavingString = JSON.stringify(memoSavingList)
+    localStorage.setItem('storagememosave', memusavingString)
+    ctMemoList.addEventListener('click',memoListsPopUp)
+    memoList.append(ctMemoList)
+}
+
+function setMemo(){
     const ifMemoInputLength = memoMainInput.value.length 
-    const ifMemoTitleLength = memoTitleInput.value.length
+         ,ifMemoTitleLength = memoTitleInput.value.length
     if(ifMemoInputLength > 0 &&  ifMemoTitleLength > 0){
-        const memoTitleInputValue = memoTitleInput.value
-        const memoMainInputValue = memoMainInput.value
-        const ctMemoList = document.createElement('li')
-        const ctdeleteinput = document.createElement('input')
-        const ctTitleValue = document.createElement('span')
-        const ctMainValue = document.createElement('span')
-        ctTitleValue.innerText = memoTitleInputValue
-        ctMainValue.innerText = memoMainInputValue
-        ctTitleValue.classList.add('inner-title')
-        ctMainValue.classList.add('inner-main')
-        ctdeleteinput.type = "checkbox"
-        ctMemoList.append(ctdeleteinput,ctTitleValue,ctMainValue)
-        ctMemoList.addEventListener('click',memoListsPopUp)
-        memoList.append(ctMemoList)
+        memoCreative(memoTitleInput.value,memoMainInput.value)
     }  
 }
 
+function loadingSaveMemos(){
+    if(localStorage.getItem('storagememosave')){
+    const storageMemoSave = JSON.parse(localStorage.getItem('storagememosave'))
+    storageMemoSave.forEach(item => {
+        memoCreative(item.ctt,item.ctm)
+    })
+   }
+}
 
 
-
+loadingSaveMemos()
 memoArea.addEventListener('submit',noevent)
-memoSave.addEventListener('click',memoSaving)
+memoSave.addEventListener('click',setMemo)
+
+
 
